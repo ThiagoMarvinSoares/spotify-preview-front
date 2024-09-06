fc77dc531d1af0dcb0045d885ef399bee38347c1
tsoares <tsoares@kryptontech.com.br>
2024-09-06 14:36:13 -0300

    Sending code retrieving playlist info

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @next/next/no-img-element */
 'use client';
 
 import { useEffect, useState } from 'react';
@@ -6,24 +8,59 @@ import ProgressBar from '../components/ProgressBar/ProgressBar';
 import Playlist from '../components/Playlist/Playlist';
 import axios from 'axios';
 
+interface response{
+    name: string;
+    description: string;
+    tracks: string;
+}
+
 export default function Home(){
     //Setting data state
-    let [data, setData] = useState(null)
+    const [data, setData] = useState <string> ('');
+    const [playlist, setPlaylist] = useState < response | null > (null);
+    const albumImage = playlist?.images[0].url;
 
         //Use effect to get the data async
         useEffect(() => {
             axios.get('http://127.0.0.1:5000/accessToken')
             .then((response) => {
                 setData(response.data)
+                getPlaylist(response.data)
             })
             .catch(() => {
                 console.log('erro:', data)
             })
-        })
+        },[])
+
+        //get playlist info
+        const getPlaylist = (data:string) => {
+            if (!data) {return}
+
+            const headers = {
+                "Authorization": `Bearer ${data}`
+            }
+
+            axios.get('https://api.spotify.com/v1/playlists/37i9dQZF1DZ06evO2k3tf2',{headers: headers})
+            .then((response) => {
+                setPlaylist(response.data)
+            })
+            .catch((error) => {
+                console.log('error', data)
+            })
+        }
+
     return (
         <div className='flex items-center justify-center h-screen'>
             <div className='flex justify-center bg-blue-500'>
-                <div className='border border-gray-400 p-2 m-2'><Playlist/></div>
+                { playlist === null ? (null) : (
+                    <div>
+                        <p>{playlist.name}</p>
+                        <img src={albumImage} alt="Image from album" className='w-48'/> 
+                    </div>
+                )}
+                <div className='border border-gray-400 p-2 m-2'>
+                    <Playlist/>
+                </div>
                 <div>
                     <div className='border border-gray-400 p-2 m-2'><Player/></div>
                     <div className='border border-gray-400 p-2 m-2'><ProgressBar/></div>    
