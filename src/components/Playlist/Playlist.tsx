6f8b239ee38fc7acb8dbf7c5dc8331b939e19f18
Pedro Augusto <hpedroaugusto@hotmail.com>
2024-09-26 18:14:05 -0300

    Added images and box overflow

--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -1,7 +1,33 @@
 import Home from "@/src/views/Home"
 
-export default function Playlist(){
+interface interfaceTrack {
+    track: { 
+        name: string;
+        album: {
+            images: { url: string }[];
+        }
+    };
+}
+
+interface PlaylistProps {
+    tracks: Array<interfaceTrack>;
+}
+
+export default function Playlist({ tracks }: PlaylistProps){
     return (
-        <h3>Playlist</h3>
-    )
+        <div className="max-h-full overflow-y-auto p-2">
+            <ul>
+                {tracks.map((track, index) => (
+                    <li key={index} className="flex items-center space-x-4">
+                        <img 
+                            src={track.track.album.images[0]?.url} 
+                            alt=""
+                            className="w-12 h-12" 
+                        />
+                        <p>{track.track.name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
