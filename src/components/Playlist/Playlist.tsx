18fc930fd7a4acdbcc238251d65a379edeea4e2d
Pedro Augusto <hpedroaugusto@hotmail.com>
2024-09-27 22:07:25 -0300

    Added hover music color

--- src/components/Playlist/Playlist.tsx
+++ src/components/Playlist/Playlist.tsx
@@ -16,13 +16,13 @@ interface PlaylistProps {
 export default function Playlist({ tracks }: PlaylistProps){
     return (
         <div className="max-h-full overflow-y-auto p-2">
-            <ul>
+            <ul className="space-y-2">
                 {tracks.map((track, index) => (
-                    <li key={index} className="flex items-center space-x-4">
+                    <li key={index} className="flex items-center space-x-4 hover:bg-slate-600">
                         <img 
                             src={track.track.album.images[0]?.url} 
                             alt=""
-                            className="w-12 h-12" 
+                            className="w-12 h-12 rounded-sm p-0.5" 
                         />
                         <p>{track.track.name}</p>
                     </li>
