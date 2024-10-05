1f082b46b47014cb2f095187c445ccd091dd4b4b
Pedro Augusto <hpedroaugusto@hotmail.com>
2024-10-05 08:05:39 -0300

    Added buttons and change player for easy understanding

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -11,20 +11,23 @@ interface PlayerProps {
 }
 
 export default function Player({ currentTrack }: PlayerProps) {
+    if (!currentTrack) {
+        return <p>No music playing</p>
+    }
+    
     return (
         <div className="flex flex-col items-center justify-center space-y-4">
-            {currentTrack ? (
-                <>
-                    <img 
-                        src={currentTrack.track.album.images[0]?.url} 
-                        alt="Music cover image"
-                        className="w-42 p-1 rounded-xl"
-                    />
-                    <p className="text-3xl">{currentTrack.track.name}</p>
-                </>    
-            ) : (
-                <p className="text-3xl">No music playing</p>              
-            )} 
+            <img 
+                src={currentTrack.track.album.images[0]?.url} 
+                alt="Music cover image"
+                className="w-42 p-1 rounded-xl"
+            />
+            <p className="text-3xl">{currentTrack.track.name}</p>
+            <div className="flex items-center justify-center space-x-4">
+                <img className="h-8 cursor-pointer" src="images/previous.svg" alt="Previous button" />                
+                <img className="h-8 cursor-pointer" src="images/play.svg" alt="Play button" />
+                <img className="h-8 cursor-pointer" src="images/next.svg" alt="Next button" />
+            </div>
         </div>
     );
 }
